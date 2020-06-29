99daadcae93f7c12d699d731a30037e0333d9056
florpi <florpisoteadadejardin@gmail.com>
2020-06-29 13:19:02 +0100

    tests pass

--- gsm/models/gaussian/from_los.py
+++ gsm/models/gaussian/from_los.py
@@ -1,11 +1,8 @@
 import numpy as np
-from streaming.moments.project_moments import project_moment_to_los
-import streaming.moments.compute_moments as cm 
 from scipy.stats import norm
 
-
 def losmoments2gaussian(mean, scale):
-    '''
+    """
     Args:
         mean: np.array
         std: np.array
@@ -14,14 +11,12 @@ def losmoments2gaussian(mean, scale):
         pdf_los: function
 
 
-    '''
+    """
 
-    def pdf_los(vlos, rperp, rparallel):
+    def pdf_los(vlos, r_perp, r_parallel):
 
-        return norm.pdf(vlos, 
-                loc = mean(rparallel, rperp), 
-                scale = scale(rpararallel, rperp)
-                )
+        return norm.pdf(
+            vlos, loc=mean(r_parallel, r_perp), scale=scale(r_parallel, r_perp)
+        )
+
     return pdf_los
-
-
--- gsm_tests/streaming_integral/test_streaming_integral.py
+++ gsm_tests/streaming_integral/test_streaming_integral.py
@@ -1,12 +1,22 @@
 import numpy as np
 import pytest
+from scipy.integrate import simps, quadrature, quad
 
+from gsm.streaming_integral  import real2redshift
 from gsm.models.gaussian import from_los as gaussian_from_los
 
 
-def test__analytical():
-    mean = lambda x: return 0
-    scale = lambda x: return 1
+@pytest.mark.parametrize("a,b", [(2, 3), (3, 2), (10, 1)])
+def test__analytical(a, b):
+    mean = lambda rparallel, rperp: 0
+    scale = lambda rparallel, rperp: 1
+    tpcf = lambda r: r**2
+    gaussian_pdf = gaussian_from_los.losmoments2gaussian(mean, scale)
+    s = np.sqrt(a**2+b**2)
+    mu = a/np.sqrt(a**2 + b**2)
+    integrand = real2redshift.integrand_s_mu(s, mu, tpcf, gaussian_pdf)
+    r_integrand = np.linspace(-100,100,100)
+    result = simps(integrand(r_integrand), r_integrand, axis=-1)
+    analytical_result = 2 + a**2 + b**2
+    assert result == pytest.approx(analytical_result, rel=0.05)
 
-    gaussian_from_los.losmoments2gaussian(mean, scale)
-    
