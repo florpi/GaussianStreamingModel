ef56224516e0c53a121e27e9cce9496d49a9df3f
florpi <florpisoteadadejardin@gmail.com>
2020-06-29 13:50:21 +0100

    better docstrings

--- gsm/models/gaussian/from_los.py
+++ gsm/models/gaussian/from_los.py
@@ -1,19 +1,21 @@
 import numpy as np
+from typing import Callable
 from scipy.stats import norm
 
-def losmoments2gaussian(mean, scale):
+def losmoments2gaussian(mean: Callable, scale: Callable)->Callable:
     """
     Args:
-        mean: np.array
-        std: np.array
+        mean: function that takes r_parallel and r_perp as inputs and returns the mean 
+        pairwise velocity
+        std:  function that takes r_parallel and r_perp as inputs and returns the standard 
+        deviation of the pairwise velocity
+
 
     Returns:
-        pdf_los: function
-
-
+        pdf_los: line of sight pairwise velocity PDF 
     """
 
-    def pdf_los(vlos, r_perp, r_parallel):
+    def pdf_los(vlos: np.array, r_perp: np.array, r_parallel: np.array):
 
         return norm.pdf(
             vlos, loc=mean(r_parallel, r_perp), scale=scale(r_parallel, r_perp)
